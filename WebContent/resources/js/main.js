b7be7337e0c33089877fd748f76ec936b5d689d2
jiwonchoi2 <jiwon.choi.2@stonybrook.edu>
2019-11-29 20:49:07 -0500

    First attempt

--- WebContent/resources/js/main.js
+++ WebContent/resources/js/main.js
@@ -1,45 +1,62 @@
-		var ny = document.getElementById("NY");
-		var ml = document.getElementById("ML");
-		var wc = document.getElementById("WC");
-		var StateNameML = statesData.features[0].properties.name; //maryland: geojson._layers[57].feature.properties.name
-		var densityML = statesData.features[0].properties.density;
-		var StateNameNY = statesData.features[1].properties.name;
-		var densityNY = statesData.features[1].properties.density;
-		var StateNameWC = statesData.features[2].properties.name;
-		var densityWC = statesData.features[2].properties.density;
-		
-		var btn = document.getElementById("myBtn");
-		//var subbtn = document.getElementById("submitBtn");
-		
-		btn.onclick = function(props) {
-			//modal.style.display = "block";
-			if(ml.checked == true){
-			document.getElementById("dataTable").innerHTML = '<h4>US Population Density</h4>' + (props ?
-                '<table><tr><th>' + '<b>name</b>' + '</th><th>' + '<b>population</b>' + '</th></tr><tr><td>' + StateNameML + '</td><td>' + densityML + '</td></tr><tr><td>' + StateNameML + '</td><td>' + densityML + '</td></tr><tr><td>' + StateNameML + '</td><td>' + densityML + '</td></tr></table>' :
-                'Hover over a state');			
-                onclick="zoomin_maryland()";	
-                console.log("ML");
-			}
-			
-			else if(ny.checked == true){
-			document.getElementById("dataTable").innerHTML = '<h4>US Population Density</h4>' + (props ?
-                '<table><tr><th>' + '<b>name</b>' + '</th><th>' + '<b>population</b>' + '</th></tr><tr><td>' + StateNameNY + '</td><td>' + densityNY + '</td></tr><tr><td>' + StateNameNY + '</td><td>' + densityNY + '</td></tr><tr><td>' + StateNameNY + '</td><td>' + densityNY + '</td></tr></table>' :
-                'Hover over a state');		
-			console.log("NY");
-			}
-			
-			else if(wc.checked == true){
-			document.getElementById("dataTable").innerHTML = '<h4>US Population Density</h4>' + (props ?
-                '<table><tr><th>' + '<b>name</b>' + '</th><th>' + '<b>population</b>' + '</th></tr><tr><td>' + StateNameWC + '</td><td>' + densityWC + '</td></tr><tr><td>' + StateNameWC + '</td><td>' + densityWC + '</td></tr><tr><td>' + StateNameWC + '</td><td>' + densityWC + '</td></tr></table>' :
-                'Hover over a state');
-				console.log("WC");
-			}
-			
-			else{
-			document.getElementById("dataTable").innerHTML = " ";
-			}
-		}
-		
+      var ny = document.getElementById("NY");
+      var ml = document.getElementById("ML");
+      var wc = document.getElementById("WC");
+      var StateNameML = statesData.features[0].properties.name; //maryland: geojson._layers[57].feature.properties.name
+      var densityML = statesData.features[0].properties.density;
+      var StateNameNY = statesData.features[1].properties.name;
+      var densityNY = statesData.features[1].properties.density;
+      var StateNameWC = statesData.features[2].properties.name;
+      var densityWC = statesData.features[2].properties.density;
+      
+      var btn_phase0 = document.getElementById("phase0_button");
+      //var subbtn = document.getElementById("submitBtn");
+      
+      btn_phase0.onclick = function(props) {
+         
+         var developerData = getSelectedState(props);
+         $.ajax({
+             type : "POST",
+             contentType : "application/json",
+             url : "/spring-mvc-demo/api/getState",
+             data : JSON.stringify(developerData),
+             dataType : 'json',      
+             success : function(data) {
+                console.log(data);
+             }
+          });
+         
+      }
+      
+      function getSelectedState(props){
+         var data = {"stateName" : StateNameWC};
+         if(ml.checked == true){
+                document.getElementById("dataTable").innerHTML = '<h4>US Population Density</h4>' + (props ?
+                       '<table><tr><th>' + '<b>name</b>' + '</th><th>' + '<b>population</b>' + '</th></tr><tr><td>' + StateNameML + '</td><td>' + densityML + '</td></tr><tr><td>' + StateNameML + '</td><td>' + densityML + '</td></tr><tr><td>' + StateNameML + '</td><td>' + densityML + '</td></tr></table>' :
+                       'Hover over a state');         
+                       onclick="zoomin_maryland()"; 
+                       data["stateName"] = StateNameML;
+                }
+                
+                else if(ny.checked == true){
+                document.getElementById("dataTable").innerHTML = '<h4>US Population Density</h4>' + (props ?
+                       '<table><tr><th>' + '<b>name</b>' + '</th><th>' + '<b>population</b>' + '</th></tr><tr><td>' + StateNameNY + '</td><td>' + densityNY + '</td></tr><tr><td>' + StateNameNY + '</td><td>' + densityNY + '</td></tr><tr><td>' + StateNameNY + '</td><td>' + densityNY + '</td></tr></table>' :
+                       'Hover over a state');      
+                      data["stateName"] = StateNameML;
+                }
+                
+                else if(wc.checked == true){
+                document.getElementById("dataTable").innerHTML = '<h4>US Population Density</h4>' + (props ?
+                       '<table><tr><th>' + '<b>name</b>' + '</th><th>' + '<b>population</b>' + '</th></tr><tr><td>' + StateNameWC + '</td><td>' + densityWC + '</td></tr><tr><td>' + StateNameWC + '</td><td>' + densityWC + '</td></tr><tr><td>' + StateNameWC + '</td><td>' + densityWC + '</td></tr></table>' :
+                       'Hover over a state');
+                      data["stateName"] = StateNameWC;
+                }
+                
+                else{
+                document.getElementById("dataTable").innerHTML = " ";
+                }
+         return data;
+      }
+      
     
         function openNav() {
             document.getElementById("mySidepanel").style.width = "400px";
@@ -55,4 +72,4 @@
 
         function closeNav2() {
             document.getElementById("mySidepanel2").style.width = "0";
-        }	
+        }   
